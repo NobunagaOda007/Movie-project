dc6f6b11853fcfadb2c9ae5fda55a33cdda4ab69
NobunagaOda007 <leonardo.puhanic2@gmail.com>
2022-11-09 18:36:01 +0100

    added path to backend

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-if(process.env.NODE_ENV !== 'production'){}else{
+if(process.env.NODE_ENV !== 'production'){'https://backend-for-movies.adaptable.app/'}else{
     axios.defaults.baseURL = 'http://localhost:5000/';
 }
 axios.defaults.withCredentials= true;
