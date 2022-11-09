c718220c1ffc32196567e8885f0c5d3d1a3c361b
NobunagaOda007 <leonardo.puhanic2@gmail.com>
2022-11-09 20:11:03 +0100

    .

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 if(process.env.NODE_ENV !== 'production'){}else{
- //   axios.defaults.baseURL = 'http://localhost:5000/';
+    axios.defaults.baseURL = 'http://localhost:5000/';
 };
-axios.defaults.baseURL ='https://backend-for-movies.adaptable.app/';
+// axios.defaults.baseURL ='https://backend-for-movies.adaptable.app/';
 axios.defaults.withCredentials= true;
