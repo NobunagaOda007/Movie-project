58c8a0f877b5b1b11e87ab63dd0bb653cf08c629
NobunagaOda007 <leonardo.puhanic2@gmail.com>
2022-11-09 18:46:47 +0100

    axios contacting backend

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-if(process.env.NODE_ENV !== 'production'){'https://backend-for-movies.adaptable.app/'}else{
-    axios.defaults.baseURL = 'http://localhost:5000/';
-}
+if(process.env.NODE_ENV !== 'production'){}else{
+ //   axios.defaults.baseURL = 'http://localhost:5000/';
+};
+axios.defaults.baseURL ='https://backend-for-movies.adaptable.app/';
 axios.defaults.withCredentials= true;
